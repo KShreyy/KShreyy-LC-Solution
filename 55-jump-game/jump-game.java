68f3d5a9750ac71be7fe8d60c8898f68ffe50937
KShreyy <152439273+KShreyy@users.noreply.github.com>
2025-03-23 18:07:06 +0530

    Time: 2 ms (80.16%) | Memory: 46 MB (30.48%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(max>nums.length){
+                return true;
+            }
+            if(i>max){
+                return false;
+            }
+            max=Math.max(max,i+nums[i]);
+        }
+    return true;
+    }
+}
