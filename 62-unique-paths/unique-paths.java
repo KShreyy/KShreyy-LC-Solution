35e92c560fc26a914ed116a1d5f128a33f96c88f
KShreyy <152439273+KShreyy@users.noreply.github.com>
2025-06-12 16:53:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (98.08%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,6 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] t=new int[m][n];
+        for(int i=0;i<n;i++){
+            t[0][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            t[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                
+                t[i][j]=t[i-1][j]+t[i][j-1];}
+            }
+        
+        return t[m-1][n-1];
+    }}
+/*class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] t=new int[m][n];
         return helper(m-1,n-1,t);
         
     }
@@ -18,4 +35,4 @@ class Solution {
         
         return t[i][j]=sum;
     }
-}
+}*/
