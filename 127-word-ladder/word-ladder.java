ef481b4f426cefb32c8f7342529c3de97c003f05
KShreyy <152439273+KShreyy@users.noreply.github.com>
2025-07-02 22:13:35 +0530

    Time: 66 ms (82.60%) | Memory: 46.7 MB (58.55%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,73 +1,94 @@
 import java.util.*;
 
+class Pair {
+    String str;
+    int num;
+    
+    Pair(String str, int num) {
+        this.str = str;
+        this.num = num;
+    }
+}
+
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        // Create a queue for BFS, storing pairs of (word, steps)
-        Queue<Pair<String, Integer>> queue = new LinkedList<>();
-        queue.add(new Pair<>(beginWord, 1)); // Start with the beginWord and step count 1
+        // Convert list to set for O(1) lookup
+        Set<String> set = new HashSet<>(wordList);
+        
+        if (!set.contains(endWord)) return 0;  // endWord must be in the list
 
-        // Convert wordList to a set for O(1) lookups and deletions
-        Set<String> wordSet = new HashSet<>(wordList);
-
-        // If the beginWord is in the set, remove it to avoid revisiting
-        if (wordSet.contains(beginWord)) {
-            wordSet.remove(beginWord);
-        }
-
-        // Perform BFS
-        while (!queue.isEmpty()) {
-            Pair<String, Integer> current = queue.poll();
-            String word = current.getKey();
-            int steps = current.getValue();
-
-            // If the current word matches the endWord, return the steps
-            if (word.equals(endWord)) {
-                return steps;
-            }
-
-            // Try changing each character of the word to all possible letters (a-z)
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(beginWord, 1));
+        
+        while (!q.isEmpty()) {
+            Pair element = q.poll();
+            String word = element.str;
+            int level = element.num;
+            
+            if (word.equals(endWord)) return level;
+            
             for (int i = 0; i < word.length(); i++) {
-                char[] wordArray = word.toCharArray(); // Convert the word to a char array for easy manipulation
-                char originalChar = wordArray[i]; // Save the original character
-
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    wordArray[i] = ch; // Replace the character at position i with ch
-                    String newWord = new String(wordArray); // Convert the char array back to a string
-
-                    // If the new word exists in the wordSet, add it to the queue
-                    if (wordSet.contains(newWord)) {
-                        wordSet.remove(newWord); // Mark as visited
-                        queue.add(new Pair<>(newWord, steps + 1)); // Add to queue with incremented steps
+                char[] chars = word.toCharArray();
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (chars[i] == c) continue;  // avoid unnecessary work
+                    
+                    chars[i] = c;
+                    String newWord = new String(chars);
+                    
+                    if (set.contains(newWord)) {
+                        q.add(new Pair(newWord, level + 1));
+                        set.remove(newWord);  // mark as visited
                     }
                 }
-
-                // Restore the original character for the next iteration
-                wordArray[i] = originalChar;
             }
         }
-
-        // If no transformation sequence is found, return 0
-        return 0;
+        
+        return 0;  // if endWord not reachable
     }
 }
 
-// Custom Pair class since Java does not have a built-in Pair class
-class Pair<K, V> {
-    private K key;
-    private V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
+/*
+class Pair{
+    String str;
+    int num;
+    Pair(String str,int num){
+        this.str=str;
+        this.num=num;
     }
 }
+class Solution {
+    pubic pair(String str,int count){}
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        //making a set ds to stoe the wordlist for less tC linear search
+        Set<String> set=new Hashset<>();
+        for(String element:wordList){
+            set.add(element);
+        }
+        
+        //make a queueu to do bfs
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(){beginword,1});
 
-// Driver code
+        //start a for loop 
+        while(!q.isEmpty()){
+            Pair element=q.poll();
+            String s=element.str;
+            int val=element.num;
+            for(int i=0;i<s.length();i++){
+                 char ch=s.charAt(i);
+                 for(char ch='a';ch<='z';ch++){
+                    String new_string=s.substring(0,i)+ch+s.substring(i+1,s.length());
+                    
+                    if(set.contains(new_string)){
+                        count=count+1;
+                        q.add(Pair(){new_string,count});
+                    }
+                 }
+            }
+
+        }
+
+    }
+}
+*/
